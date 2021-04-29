df555b1c6b27446bdd46d0922dc9b7dc698ac428
EpiTorres <e.torres.smith@gmail.com>
2021-04-29 14:29:03 -0500

    Added event handlers

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@
  * handles window resizes.
  *
  */
-import { WebGLRenderer, PerspectiveCamera, Vector3 } from 'three';
+import { WebGLRenderer, PerspectiveCamera, Vector3, Vector2 } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { SeedScene } from 'scenes';
 
@@ -37,6 +37,16 @@ controls.update();
 
 // Render loop
 const onAnimationFrameHandler = (timeStamp) => {
+
+    /////////////////////////////////////////////
+    if (counter > delay && initGrapple) {
+        initGrapple = false;
+        isGrappling = true;
+        console.log("is grappling");
+    }
+    else counter++;
+    /////////////////////////////////////////////
+
     controls.update();
     renderer.render(scene, camera);
     scene.update && scene.update(timeStamp);
@@ -53,3 +63,82 @@ const windowResizeHandler = () => {
 };
 windowResizeHandler();
 window.addEventListener('resize', windowResizeHandler, false);
+
+
+/////////////////////////////////////////////////////////////////////
+// First draft of event listeners
+
+var initGrapple = false; // Whenever the raycast should be initialized
+var isGrappling = false; // Whenever the grappling is happening
+var delay = 20; // number of timesteps before the grapple is initialized
+var counter = 0; // counter to keep track of the current delay
+var grappleRay;
+// For grappling
+const initiateGrapplingHook = (event) => {
+    // Left click
+    if (event.button == 0) {
+        const pos = camera.position;
+        const direction = new Vector3();
+        camera.getWorldDirection(direction);
+        grappleRay = {origin: pos, direction: direction};
+        console.log(grappleRay);
+        initGrapple = true;
+        isGrappling = false;
+        counter = 0;
+        console.log("init grapple");
+    }
+    // Other mouse clicks
+    else if (isGrappling) {
+        movingTowardGrapple = true;
+        console.log("moving toward grapple");
+    }
+    
+}
+
+window.addEventListener('mousedown', initiateGrapplingHook, false);
+
+const releaseGrapplingHook = (event) => {
+        // Left click
+    if (event.button == 0) {
+        initGrapple = false;
+        isGrappling = false;
+        movingTowardGrapple = false;
+        console.log("release grapple");
+    }
+    // Other mouse clicks
+    else if (isGrappling) {
+        movingTowardGrapple = false;
+        console.log("not moving toward grapple");
+    }
+}
+window.addEventListener('mouseup', releaseGrapplingHook, false);
+
+var moveSpeed = 1;
+// var jumpSpeed = 1;
+// var isJumping = false;
+var movingTowardGrapple = false;
+
+const handlePlayerMovement = (event) => {
+    const keyMap = {
+        // Forward
+        "ArrowUp": new Vector3(0, 0, moveSpeed),
+        "w": new Vector3(0, 0, moveSpeed),
+        // Left
+        "ArrowLeft": new Vector3(moveSpeed, 0, 0),
+        "a": new Vector3(moveSpeed, 0, 0),
+        // Backward
+        "ArrowDown": new Vector3(0, 0, -moveSpeed),
+        "s": new Vector3(0, 0, -moveSpeed),
+        // Right
+        "ArrowRight": new Vector3(-moveSpeed, 0, 0),
+        "d": new Vector3(-moveSpeed, 0, 0),
+    }
+    const vec = keyMap[event.key];
+    console.log(vec);
+
+    // Still need to handle jumping when pressing spacebar.
+    // Still need to handle moving towards grapple when
+    // pressing shift.
+}
+window.addEventListener('keydown', handlePlayerMovement, false);
+/////////////////////////////////////////////////////////////////////
